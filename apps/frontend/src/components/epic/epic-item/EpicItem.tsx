7322b91a56fbafd801b36cb9f4881bad245024c5
Theodore Chhean <theo.chhean@gmail.com>
2026-09-10 10:20:36 +0200

    refactor(frontend): mutualize roadmap and voting cards into DetailCard and updated styling (spaces, line clamp)

--- apps/frontend/src/components/epic/epic-item/EpicItem.tsx
+++ apps/frontend/src/components/epic/epic-item/EpicItem.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { EpicItemCard } from '@/components/epic/epic-item/EpicItemCard';
 import { EpicItemDetailed } from '@/components/epic/epic-item/EpicItemDetailed';
+import { useDetailParam } from '@/hooks/use-detail-param';
 import { Dialog, DialogContent } from '@filigran/ui';
 import { epic_fragment$data } from '@generated/epic_fragment.graphql';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useCallback } from 'react';
 
 interface EpicItemProps {
   epic: epic_fragment$data;
@@ -19,29 +18,10 @@ export const EpicItem = ({
   userCanUpdate,
   userCanDelete,
 }: EpicItemProps) => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const isOpen = searchParams.get('epicId') === epic.id;
-
-  const handleClose = useCallback(
-    (open: boolean) => {
-      if (!open) {
-        const params = new URLSearchParams(searchParams.toString());
-        params.delete('epicId');
-
-        const queryString = params.toString();
-        const targetUrl = queryString ? `${pathname}?${queryString}` : pathname;
-
-        router.replace(targetUrl, { scroll: false });
-      }
-    },
-    [router, pathname, searchParams]
-  );
+  const { isOpen, close } = useDetailParam('epicId', epic.id);
 
   return (
-    <li className="group overflow-hidden flex flex-col relative rounded hover:cursor-pointer bg-elevation-background-layer-1 h-[183px]">
+    <li className="h-full">
       <EpicItemCard
         epic={epic}
         serviceInstanceId={serviceInstanceId}
@@ -50,7 +30,7 @@ export const EpicItem = ({
       />
       <Dialog
         open={isOpen}
-        onOpenChange={handleClose}>
+        onOpenChange={(open) => !open && close()}>
         <DialogContent className="p-0 w-full max-w-5xl h-[80vh] max-h-[90vh] flex flex-col overflow-hidden">
           <EpicItemDetailed
             epic={epic}
