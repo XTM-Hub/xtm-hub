1481cf6f42c16118e54f5c4d348f2a95af8e116f
Valentin <6004961+Kroustille@users.noreply.github.com>
2026-09-21 13:37:10 +0000

    feat(news-feed): handle news feeds for integration except connectors (#2404) (#3455)
    feat(news-feed): handle news feeds for integration except connectors

--- apps/backend/src/modules/news-feed/news-feed.helper.ts
+++ apps/backend/src/modules/news-feed/news-feed.helper.ts
@@ -1,6 +1,15 @@
-import { PlatformIdentifier } from '../../__generated__/resolvers-types';
+import {
+  PlatformIdentifier,
+  ServiceDefinitionIdentifier,
+} from '../../__generated__/resolvers-types';
+import Document from '../../model/kanel/public/Document';
 import { RequiredPlatformVersions } from '../../utils/required-platform-version';
 import { doesVersionSatisfy, isValidVersion } from '../../utils/versioning';
+import { isConnectorDocument } from '../shareable-resource/opencti/integration/integration.model';
+import {
+  NewsFeedConfiguration,
+  newsFeedConfigurationMapping,
+} from './news-feed.model';
 
 export const doesPlatformSupportNewsFeed = (
   identifier: PlatformIdentifier,
@@ -10,4 +19,23 @@ export const doesPlatformSupportNewsFeed = (
   if (!requiredVersion) return true;
   if (!version || !isValidVersion(version)) return false;
   return doesVersionSatisfy({ givenVersion: version, requiredVersion });
+};
+
+export const NewsFeedHelper = {
+  getNewsFeedConfiguration: (
+    serviceDefinitionIdentifier: ServiceDefinitionIdentifier,
+    document: Document
+  ): NewsFeedConfiguration | undefined => {
+    const newsFeedConfiguration =
+      newsFeedConfigurationMapping[serviceDefinitionIdentifier];
+    if (!newsFeedConfiguration) {
+      return undefined;
+    }
+
+    if (isConnectorDocument(document)) {
+      return undefined;
+    }
+
+    return newsFeedConfiguration;
+  },
 };
--- apps/frontend/graphql/generated.ts
+++ apps/frontend/graphql/generated.ts
@@ -1564,6 +1564,7 @@ export enum NewsFeedItemMetadataKey {
 export enum NewsFeedItemType {
   ResourceCustomDashboard = 'RESOURCE_CUSTOM_DASHBOARD',
   ResourceCustomView = 'RESOURCE_CUSTOM_VIEW',
+  ResourceIntegration = 'RESOURCE_INTEGRATION',
   ResourcePlaybook = 'RESOURCE_PLAYBOOK'
 }
 
